a4258a7db6ea8f08333934a4ac68b7b8f95261f5
Louis Wich <lwi@intomics.com>
2016-10-27 11:37:23 +0200

    Bug: Error not caught on timeout and or internet down. Now call errback on xhr no response (status 0).

--- text.js
+++ text.js
@@ -306,9 +306,14 @@ define(['module'], function (module) {
                 //visible via console output in the browser.
                 if (xhr.readyState === 4) {
                     status = xhr.status || 0;
-                    if (status > 399 && status < 600) {
-                        //An http 4xx or 5xx error. Signal an error.
-                        err = new Error(url + ' HTTP status: ' + status);
+                    if (status > 399 && status < 600 || status === 0) {
+                        if (status===0) {
+                            // 0 signal no response
+                            err = new Error(url + ' No response');
+                        } else {
+                            //An http 4xx or 5xx error. Signal an error. 
+                            err = new Error(url + ' HTTP status: ' + status);
+                        }
                         err.xhr = xhr;
                         if (errback) {
                             errback(err);
